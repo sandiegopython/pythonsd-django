a36787ca2e6f3932fbbe84a240fef3b51235bd11
Micah Denbraver <macromicah@gmail.com>
2015-05-10 15:39:16 -0700

    Add some static pages (chat, CoC)

--- pythonsd/urls.py
+++ pythonsd/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, static, url
 from django.contrib import admin
+from django.views import generic
 
 from homepage.views import home_view
 
 urlpatterns = [
     url(r'^$', home_view, name='home'),
+    url(r'^chat-room.html$', generic.TemplateView.as_view(template_name='pythonsd/pages/chat-room.html'), name='chat-room'),
+    url(r'^code-of-conduct.html$', generic.TemplateView.as_view(template_name='pythonsd/pages/code-of-conduct.html'), name='code-of-conduct'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
