b16bc513d4262e5df6d20cc7383fcaf49484beaa
Micah Denbraver <macromicah@gmail.com>
2015-11-07 21:52:22 -0800

    Update static path

--- pythonsd/settings.py
+++ pythonsd/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'pythonsd', 'static'),)
 
