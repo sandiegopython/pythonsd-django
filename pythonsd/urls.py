b27bbf409ca57568f5821afbc407225d2965aa40
Micah Denbraver <macromicah@gmail.com>
2015-11-21 11:26:23 -0800

    Remove development 'media' file handling (unused at this time)

--- pythonsd/urls.py
+++ pythonsd/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, static, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views import generic
 from revproxy.views import ProxyView
@@ -10,9 +10,3 @@ urlpatterns = [
     url(r'^$', generic.RedirectView.as_view(url='index.html', permanent=False), name='home'),
     url(r'^(?P<path>.*)$', ProxyView.as_view(upstream=settings.PYTHONSD_STATIC_SITE))
 ]
-
-if settings.DEBUG:
-    urlpatterns.extend(static.static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    ))
