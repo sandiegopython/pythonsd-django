97137450cf6a97bc31920da5ff1a22eada430363
David Fischer <djfische@gmail.com>
2019-10-06 17:07:06 -0700

    Update the dependencies and runtime
    - Python 3.5 -> 3.7
    - Django 1.9 -> 1.11
    - Swaps dj-static for whitenoise (current Heroku recommendation)

--- pythonsd/static_files.py
+++ pythonsd/static_files.py
@@ -5,6 +5,9 @@ import tasks
 
 class CompileFinder(BaseFinder):
 
+    def find(self, path, all=False):
+        return []
+
     def list(self, ignore_patterns):
         """Perform the compile action."""
         self.compile()
