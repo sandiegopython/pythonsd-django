a0a8bf387950ad336ec9459c3ad38f1eeea82caf
David Fischer <djfische@gmail.com>
2019-10-11 10:16:15 -0700

    Format changes based on code style

--- pythonsd/views.py
+++ pythonsd/views.py
@@ -21,40 +21,40 @@ class MeetupWidget(TemplateView):
     """
 
     cache_duration = 60 * 15
-    cache_key = 'pythonsd.views.MeetupWidget'
-    http_method_names = ['get', 'head', 'options']
-    template_name = 'pythonsd/meetup-widget.html'
+    cache_key = "pythonsd.views.MeetupWidget"
+    http_method_names = ["get", "head", "options"]
+    template_name = "pythonsd/meetup-widget.html"
 
     def get(self, request, *args, **kwargs):
-        format = kwargs.get('format')
+        format = kwargs.get("format")
 
-        if format == 'json':
+        if format == "json":
             response = JsonResponse(self.get_upcoming_events(), safe=False)
         else:
             response = super().get(self, request, *args, **kwargs)
 
-        response['Access-Control-Allow-Origin'] = '*'
+        response["Access-Control-Allow-Origin"] = "*"
 
         return response
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['upcoming_events'] = self.get_upcoming_events()
+        context["upcoming_events"] = self.get_upcoming_events()
         return context
 
     def get_upcoming_events(self):
         events = cache.get(self.cache_key)
         if events:
-            log.debug('Using Meetup.com events from cache')
+            log.debug("Using Meetup.com events from cache")
             return events
 
-        log.debug('Requesting upcoming events from Meetup.com')
+        log.debug("Requesting upcoming events from Meetup.com")
 
         # https://www.meetup.com/meetup_api/docs/:urlname/events/
         try:
             resp = requests.get(
-                'https://api.meetup.com/pythonsd/events',
-                params={'photo-host': 'public', 'page': '3'},
+                "https://api.meetup.com/pythonsd/events",
+                params={"photo-host": "public", "page": "3"},
                 timeout=5,
             )
         except Exception:
@@ -63,12 +63,13 @@ class MeetupWidget(TemplateView):
             # Transform from meetup's API format into our format
             events = [
                 {
-                    'link': e['link'],
-                    'name': e['name'],
+                    "link": e["link"],
+                    "name": e["name"],
                     # Always show time in local San Diego time
-                    'datetime': datetime.utcfromtimestamp(e['time'] // 1000).replace(
-                        tzinfo=pytz.utc).astimezone(pytz.timezone('America/Los_Angeles')),
-                    'venue': e['venue']['name'] if 'venue' in e else None,
+                    "datetime": datetime.utcfromtimestamp(e["time"] // 1000)
+                    .replace(tzinfo=pytz.utc)
+                    .astimezone(pytz.timezone("America/Los_Angeles")),
+                    "venue": e["venue"]["name"] if "venue" in e else None,
                 }
                 for e in resp.json()
             ]
